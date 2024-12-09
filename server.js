3fb587684dc58bad4ab5c7c89a248c86d6fdefeb
Chip Dong Lim <lcdvirgo@gmail.com>
2024-12-10 01:33:28 +0800

    fixed Socket.IO client setup

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const { Server } = require("socket.io");
 
 const io = new Server(server, {
     cors: {
-        origin: "https://chipgrace-quiz-game.netlify.app",
-        methods: ["GET", "POST"],
-        credentials: true,
-        allowedHeaders: ["Access-Control-Allow-Origin"]
+        origin: ["https://chipgrace-quiz-game.netlify.app", "http://localhost:3000"],
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true
     }
 });
 
