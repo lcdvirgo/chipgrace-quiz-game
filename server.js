5011313a761dbb83b1489f35c7686382a2643233
Chip Dong Lim <lcdvirgo@gmail.com>
2024-12-13 16:19:36 +0800

    fixed deployment error

--- server.js
+++ server.js
@@ -89,12 +89,12 @@ const quizQuestions = [
         question: "How many children do we want?",
         answers: ["Stop at one", "Sky's the limit!", "2", "3"],
         correct: 2
-    }
+    },
     {
         question: "Which of the following is an activity Chip and Grace is most unlikely to do together?",
         answers: ["Skydiving", "Swimming", "Snorkelling", "Bungeejumping"],
         correct: 1
-    }
+    },
     {
         question: "Who exercises more?",
         answers: ["Chip", "Grace"],
